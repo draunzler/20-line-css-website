1a8ec016532aa5fdbba1c9826dc2555e0e28f46b
Draunzler <54343288+draunzler@users.noreply.github.com>
2023-04-20 19:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const track = document.getElementById("image-track");
+const track = document.getElementById("image-track");
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
@@ -12,50 +12,13 @@ window.onmousemove = e => {
     maxDelta = window.innerWidth / 2;
     const percentage = (mouseDelta / maxDelta) * -100,
     nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-    const boundedPercentage = Math.min(Math.max(nextPercentage, 0), -100);
-    track.dataset.percentage = boundedPercentage;
+    track.dataset.percentage = nextPercentage;
     track.animate({
-        transform: `translate(${boundedPercentage}%, -50%)`
+        transform: `translate(${nextPercentage}%, -50%)`
     }, {duration: 1200, fill: "forwards"});
     for(const image of track.getElementsByClassName("image")){
         image.animate({
-            objectPosition: `${boundedPercentage + 100}% 50%`
+            objectPosition: `${nextPercentage + 100}% 50%`
         }, {duration: 1200, fill: "forwards"});
     }
-}*/
-
-const track = document.getElementById("image-track");
-const images = track.getElementsByClassName("image");
-const imageWidth = images[0].offsetWidth;
-const trackWidth = imageWidth * images.length;
-const maxDelta = window.innerWidth / 2 - imageWidth / 2;
-const minPercentage = -100 * (images.length - 1);
-const maxPercentage = 0;
-
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-};
-
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-};
-
-window.onmousemove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-    let percentage = (mouseDelta / maxDelta) * 100;
-    if(track.dataset.prevPercentage){
-        percentage += parseFloat(track.dataset.prevPercentage);
-    }
-    percentage = Math.max(Math.min(percentage, maxPercentage), minPercentage);
-    track.dataset.percentage = percentage;
-    track.animate({
-        transform: `translate(${percentage}%, -50%)`
-    }, {duration: 1200, fill: "forwards"});
-    for(const image of images){
-        image.animate({
-            objectPosition: `${percentage + 100}% 50%`
-        }, {duration: 1200, fill: "forwards"});
-    }
-};
+}
