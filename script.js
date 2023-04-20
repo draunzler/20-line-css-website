6f251b6793fcf3af1e8baef88bfa198b0553f1e6
Draunzler <shadymeister2017@gmail.com>
2023-04-20 17:27:48 +0530

    update

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const track = document.getElementById("image-track");
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
 window.onmousemove = e => {
     if(track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
-    const percentage = (mouseDelta / maxDelta)*-100;
-    track.style.transform = `translate(${percentage}%, -50%)`;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
+    maxDelta = window.innerWidth / 2;
+    const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    track.dataset.percentage = nextPercentage;
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+    for(const image of track.getElementsByClassName("image")) {
+        image.style.objectPosition = `${nextPercentage + 100} 50%`
+    }
 }
