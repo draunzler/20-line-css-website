59aec1d8fefa706a8634991409bcb83d69b57c2b
Draunzler <54343288+draunzler@users.noreply.github.com>
2023-04-30 00:03:28 +0530

    fixed slider limit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const track = document.getElementById("image-track");
+const images = track.getElementsByClassName("image");
+const totalWidth = images.length * images[0].clientWidth;
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
@@ -8,15 +10,20 @@ window.onmouseup = () => {
 }
 window.onmousemove = e => {
     if(track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
-    maxDelta = window.innerWidth / 2;
-    const percentage = (mouseDelta / maxDelta) * -100,
-    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    const maxDelta = totalWidth - window.innerWidth;
+    const percentage = (mouseDelta / maxDelta) * 100;
+    let nextPercentage = parseFloat(track.dataset.prevPercentage) - percentage;
+    if (nextPercentage > 0) {
+        nextPercentage = 0;
+    } else if (nextPercentage < -100){
+        nextPercentage = -100;
+    }
     track.dataset.percentage = nextPercentage;
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
     }, {duration: 1200, fill: "forwards"});
-    for(const image of track.getElementsByClassName("image")){
+    for(const image of images){
         image.animate({
             objectPosition: `${nextPercentage + 100}% 50%`
         }, {duration: 1200, fill: "forwards"});
